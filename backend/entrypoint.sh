e0b4844b91b13cd897e17fe665c1936686d06830
zhu0117 <mn22hz2@leeds.ac.uk>
2025-03-10 06:31:57 +0000

    chore: 清空 backend 文件夹，准备重新组织代码

--- backend/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar target/hello-0.0.1-SNAPSHOT.jar
--- backend/src/main/java/com/example/hello/service/ScooterService.java
+++
@@ -1,8 +0,0 @@
-package com.example.hello.service;
-
-import com.example.hello.entity.Scooter;
-import java.util.List;
-
-public interface ScooterService {
-    List<Scooter> findAllAvailable();
-} 
